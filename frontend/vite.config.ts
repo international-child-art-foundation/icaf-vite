c89f7c8fef94fb3bd5138c4533d8564465cda0f2
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2025-05-16 17:27:41 -0400

    feat: Implement monorepo
    * feat: Monorepo first draft
    
    * feat: Implement monorepo
    
    * fix: README formatting, declutter
    
    * fix: one period in README

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: './',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@icaf/shared': path.resolve(__dirname, '../shared/src'),
     },
   },
 });

634e592bb5fd9da1c957eaff340ec7e551c722c8
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2025-11-24 13:53:30 -0500

    feat: Professionals page

--- frontend/src/pages/Professionals.tsx
+++ frontend/src/pages/Professionals.tsx
@@ -2,15 +2,20 @@ import { ProfessionalsContact } from '@/components/professionals/ProfessionalsCo
 import { ProfessionalsHeader } from '@/components/professionals/ProfessionalsHeader';
 import { ProfessionalsHowTo } from '@/components/professionals/ProfessionalsHowTo';
 import { ProfessionalsIntro } from '@/components/professionals/ProfessionalsIntro';
+import faceCollage from '@/assets/professionals/pexels-vanessa-loring-7869442 3.webp';
 
 export const Professionals = () => {
   return (
     <div>
-      <div>
-        <ProfessionalsHeader />
+      <ProfessionalsHeader />
+      <div className="flex flex-col gap-12">
         <ProfessionalsIntro />
         <ProfessionalsHowTo />
         <ProfessionalsContact />
+        <div className="flex w-full justify-center overflow-hidden">
+          <img src={faceCollage} className="w-full min-w-[1000px]" />
+        </div>
+        <div className="h-8"></div>
       </div>
     </div>
   );

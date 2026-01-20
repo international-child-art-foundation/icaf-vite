dc6525703f8615c240ac44024260b0019a620f6c
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2026-01-20 16:50:53 -0500

    chore: Fix image and update text positioning

--- frontend/src/components/leadership/ImpactCallout.tsx
+++ frontend/src/components/leadership/ImpactCallout.tsx
@@ -7,14 +7,14 @@ export const ImpactCallout = () => {
     <div className="h-[700px] overflow-hidden rounded-[40px] bg-[#FFECCB] lg:h-[300px] xl:h-[unset]">
       <div className="grid h-full grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
         <div className="mx-auto my-auto flex flex-col gap-6 p-8">
-          <div>
+          <div className="text-center md:text-start">
             <h4 className="font-montserrat text-2xl font-extrabold">
               Investing In Childhood Worldwide
             </h4>
             <p className="text-2xl">Since 1997.</p>
           </div>
-          <Link to="/impact" className="mr-auto flex">
-            <Button className="rounded-full px-6 py-6 text-[19px]">
+          <Link to="/impact" className="flex md:mr-auto">
+            <Button className="md:mx-unset mx-auto rounded-full px-6 py-6 text-[19px]">
               View our impact
             </Button>
           </Link>
